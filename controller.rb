4eaf5625d78ca90a0aa1870846d0071d6964f60a
Stan Tarnev <stan.tarnev@gmail.com>
2019-09-16 23:35:53 +0100

    basic extensions

--- controller.rb
+++ controller.rb
@@ -5,6 +5,10 @@ require('pry')
 require_relative('./models/rock_paper_scissors.rb')
 also_reload('./models/*')
 
+get '/welcome' do
+  erb (:welcome)
+end
+
 get '/:player_1/:player_2' do
   @winner = Rock_paper_scissors.compare(params[:player_1], params[:player_2])
   erb (:result)
